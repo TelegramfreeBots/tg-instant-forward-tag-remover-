000cd8ff33787d05815e70b5f30cfd716a8c1a9a
TelegramfreeBots <136962396+TelegramfreeBots@users.noreply.github.com>
2023-06-22 14:41:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import pyrogram
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+
+bot_token = os.getenv("BOT_TOKEN")
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+bughunter0 = Client(
+    "botname",
+    bot_token=bot_token,
+    api_id=api_id,
+    api_hash=api_hash
+)
+
+@bughunter0.on_message(filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text("Start Message Here")
+
+@bughunter0.on_message(filters.forwarded and filters.channel)
+async def channeltag(bot, message):
+   try:
+       forward_msg = await message.copy(message.chat.id)
+       await message.delete()
+   except:
+       await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+    
+bughunter0.run()
